138ef6c2ba0ee7801b9195cb8705999a416bab9e
Mason <masontyler747@gmail.com>
2020-06-23 17:23:56 -0400

    Initial Commit

---
+++ Remote_Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Remote_Test
+//
+//  Created by Mason Llewellyn on 6/23/20.
+//  Copyright © 2020 Facebook University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Remote_Test/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Remote_Test
+//
+//  Created by Mason Llewellyn on 6/23/20.
+//  Copyright © 2020 Facebook University. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
